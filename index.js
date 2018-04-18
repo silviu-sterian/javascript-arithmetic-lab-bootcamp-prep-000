1fec2e6c8d505bc48833f99ec25ed7cd9f7c0a8a
ssterian@hotmail.com <ssterian@hotmail.com>
2018-04-18 21:17:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,15 +16,5 @@ function inc(a) {return a++;}
 function dec(a) {return a--;}
 
 function makeInt(n) { return parseInt(n,10);}
-function preserveDecimal(n) {parseFloat(n);}
+function preserveDecimal(n) {return parseFloat(n);}
 
-
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
